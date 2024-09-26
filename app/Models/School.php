0653508a96ee04ae00a160fcd7b9d7edee70e661
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-09-27 00:43:46 +0330

    feat(models): school

--- app/Models/School.php
+++ app/Models/School.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class School extends Model
 {
     use HasFactory;
+
+    /**
+     * @return HasMany
+     */
+    public function teachers(): HasMany
+    {
+        return $this->hasMany(Teacher::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function admins(): HasMany
+    {
+        return $this->hasMany(Admin::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function classrooms(): HasMany
+    {
+        return $this->hasMany(Classroom::class);
+    }
+
 }
