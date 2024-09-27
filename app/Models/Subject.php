8ec0768f8dbd0754a6a88db2d6f5cb5c0db61294
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-09-27 16:39:08 +0330

    feat(migrations): schools + teachers

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Subject extends Model
 {
@@ -16,5 +17,13 @@ class Subject extends Model
     public function dailySchedules(): BelongsToMany
     {
         return $this->belongsToMany(DailySchedule::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function teachers(): BelongsToMany
+    {
+        return $this->belongsToMany(Teacher::class);
     }
 }
--- database/migrations/2024_09_27_130700_create_subject_teacher_table.php
+++ database/migrations/2024_09_27_130700_create_subject_teacher_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teachers', function (Blueprint $table) {
+        Schema::create('subject_teacher', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('mobile')->unique();
-            $table->string('password');
-            $table->foreignId('school_id')->constrained('schools')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('subject_teacher');
     }
 };
