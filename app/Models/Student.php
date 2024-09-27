338fb962e123678eb12e64b625389c60f5b4cae1
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-09-27 15:50:02 +0330

    feat(models): student relations

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 
 class Student extends Model
@@ -17,5 +18,37 @@ class Student extends Model
     public function school(): BelongsTo
     {
         return $this->belongsTo(School::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function chats(): HasMany
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function dailySchedules(): HasMany
+    {
+        return $this->hasMany(DailySchedule::class);
     }
 }
