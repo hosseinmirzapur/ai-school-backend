7139e128e946e54c2b0d846fd648782cf0df11b2
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-09-30 15:42:56 +0330

    feat(seeders): RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -12,6 +12,11 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role = Role::firstOrCreate([
+            'name' => 'superadmin',
+            'permissions' => ['*']
+        ]);
+
+        $this->command->getOutput()->success("Role {$role->name} created|exists.");
     }
 }
