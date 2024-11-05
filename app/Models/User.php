3324cf93ef69501573fa3cbe0fafe57e1291b38c
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-11-05 17:11:13 +0330

    feat(deploy): deployment stuff

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 /*
  * This class is dedicated to be used as Admin on Filament Panel
  */
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
